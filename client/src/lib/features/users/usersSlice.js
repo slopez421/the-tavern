c98e612cff92a6d6903431eee1ba18ef07b74fc3
Shania Lopez <slopez@fandm.edu>
2024-09-18 17:25:53 -0400

    initial users slice  and reducer created

---
+++ client/src/lib/features/users/usersSlice.js
@@ -0,0 +1,32 @@
+import {createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+//define initial state, will replace later with api call
+const initialState = [
+    { 
+        id: "1",
+        username: 'sjohnson',
+        first_name: 'Sally',
+        last_name : 'Johnson',
+    },
+    { 
+        id: "2",
+        username: 'jdoe',
+        first_name: 'Jane',
+        last_name : 'Doe',
+    },
+]
+
+// create slice and pass initial state
+const usersSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {
+        }
+    }
+)
+
+export default usersSlice.reducer
+export const selectAllUsers = (state) => state.users
+export const selectUserById = (state, userId) =>
+    state.users.find(user => user.id === userId)
+
--- client/src/lib/store.js
+++ client/src/lib/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import postsReducer from './features/posts/postsSlice'
+import usersReducer from './features/users/usersSlice'
 
 export const makeStore = () => {
     return configureStore({
         reducer: {
-            posts: postsReducer
+            posts: postsReducer,
+            users: usersReducer
         },
     })
 }
